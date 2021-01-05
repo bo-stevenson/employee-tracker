7b41f9457e6da3e357670f1274a61903e875298a
Bo Stevenson <bostevenson1027@gmail.com>
2021-01-04 19:00:23 -0600

    added app.js and changed schema and seed

---
+++ app.js
@@ -0,0 +1,165 @@
+//require dependancies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+const logo = require('asciiart-logo');
+
+
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Tank@1027",
+    database: "employee_tracker_db"
+});
+
+// connect to the mysql server
+connection.connect(function(err) {
+    if (err) throw err;
+    init();
+});
+
+//logo
+function init() {
+    const logoText = logo
+        ({
+        name: "Employee Management System",
+        font: "Colossal",
+        fontSize: "5px",
+        borderColor: "black",
+        logoColor: "red",
+        textColor: "bold-black",
+        })
+    .render();
+    console.log(logoText);
+    
+    setTimeout(() => {
+        start();
+      }, 2000);
+    
+};
+
+let rolesarray = [];
+let departmentsarray = [];
+let employeesarray = [];
+let managersarray = [];
+
+
+function start() {
+    //Run functions to make empty arrays at start
+    roleArray();
+    departmentArray();
+    employeeArray();
+    managerArray();
+    inquirer
+      .prompt({
+        name: "choice",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "View All Departments",
+          "View All Roles",
+          "View all Employees",
+          "Remove Employee",
+          "Remove Role",
+          "Remove Department",
+          "Update Employee's Role",
+          "Exit",
+        ],
+      })
+      .then(function (answer) {
+        switch (answer.choice) {
+          case "Add Department":
+            addDepartment();
+            break;
+  
+          case "Add Role":
+            addRole();
+            break;
+  
+          case "Add Employee":
+            addEmployee();
+            break;
+  
+          case "View All Departments":
+            viewDepartments();
+            break;
+  
+          case "View All Roles":
+            viewRoles();
+            break;
+  
+          case "View all Employees":
+            viewEmployees();
+            break;
+  
+          case "Remove Employee":
+            removeEmployee();
+            break;
+  
+          case "Remove Role":
+            removeRole();
+            break;
+  
+          case "Remove Department":
+            removeDepartment();
+            break;
+  
+          case "Update Employee's Role":
+            updateEmployee();
+            break;
+
+          case "Exit":
+            connection.end();
+            break;
+        }
+    });
+  }
+
+
+
+
+//array functions
+function roleArray() {
+    connection.query("SELECT * FROM role", (err, results) => {
+        if (err) throw err;
+        rolesarray = [];
+        for (let i = 0; i < results.length; i++) {
+            rolesarray.push(results[i].id + " " + results[i].title)
+        };
+    });
+};
+
+function employeeArray () {
+    connection.query("SELECT * FROM employee", (err, results) => {
+        if (err) throw err;
+        employeesarray = [];
+        for (let i = 0; i < results.length; i++) {
+            employeesarray.push(results[i].id + " " + results[i].first_name + " " + results[i].last_name)
+        };
+    });
+};
+
+function departmentArray() {
+    connection.query("SELECT * FROM department", (err, results) => {
+        if (err) throw err;
+        departmentsarray = [];
+        for (let i = 0; i < results.length; i++) {
+            departmentsarray.push(results[i].id + " " + results[i].dept_name)
+        };
+    });
+};
+
+function managerArray() {
+    connection.query("SELECT * FROM employee WHERE manager_id IS null", (err, results) => {
+        if (err) throw err;
+        managersarray = [];
+        for (let i = 0; i < results.length; i++) {
+            managersarray.push(results[i].id + " " + results[i].first_name + " " + results[i].last_name)
+        };
+    });
+};
--- db/seed.sql
+++ db/seed.sql
@@ -1,38 +1,45 @@
 INSERT INTO department (dept_name)
-VALUES ('Sales'),
-	('Accounting'),
-    ('Human Resources'),
-    ('Legal');
-    
+VALUES ('Higher Ups'),	
+		('Sales'),
+		('Finance'),
+		('Test Subjects'),
+		('Human Resources'),
+		('Legal');
+       
 INSERT INTO employee_tracker_db.role (title, salary, department_id)
-	VALUES ('Sales Manager', 65000, 1),
-			('Sales Associate', 50000, 1),
-            ('Sales Intern', 35000, 1),
-            ('Financial Manager', 100000, 2),
-            ('Financial Advisor', 95000, 2),
-            ('HR Manager', 75000, 3),
-            ('HR Representative', 69000, 3),
-            ('Head of Legal Affairs', 150000, 4),
-            ('Legal Intern', 40000, 4);
-            
+VALUES ('Founder', 200000, 1),
+		('CEO',150000,1),
+       ('CFO', 110000, 1),
+       ('Sales Manager', 83000, 2),
+       ('Sales Associate', 57000, 2),
+       ('Sales Intern', 42000, 2),
+       ('Financial Manager', 85000, 3),
+       ('Financial Analyst', 75000, 3),
+       ('Financial Advisor', 67000, 3),
+       ('Lead Guinea Pig', 23000, 4),
+       ('Guinea Pig Associate', 18000, 4),
+       ('Intern Guinea Pig', 15000, 4),
+       ('Head of HR', 65000, 5),
+       ('HR Administrator', 42000, 5),
+       ('HR Intern', 35000, 5),
+       ('Head of Legal', 100000, 6),
+       ('Corporate Lawyer', 85000, 6),
+       ('Legal Intern', 45000, 6);
+     
 INSERT INTO employee(first_name, last_name, role_id)
-	VALUES 
-		('Jack', 'Larson', 2),
-		('Nick', 'Manning', 3),
-        ('Bryce', 'Berglund', 6),
-        ('Michael', 'Scott', 8),
-        ('Norville', 'Rogers', 9);
-        
+VALUES('Jack', 'Larson', 1),
+      ('Nick', 'Manning', 4),
+      ('Bryce', 'Berglund', 7),
+      ('Alex', 'Masberg', 10),
+      ('Nick', 'Gilmartin', 13),
+      ('Jeremy', 'Monke', 16);
+
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-        VALUES 
-        ('Bo', 'Stevenson', 1, 1),
-        ('Nathan', 'Bidinger', 44, 2),
-        ('Darth', 'Vader', 11, 3),
-        ('Joe', 'Yetzer', 15, 4);
-        
-        
-        
-		
-            
-    
-    
+VALUES('Bo', 'Stevenson', 2, 1),
+      ('Nathan', 'Bidinger', 5, 2),
+	  ('Joe', 'Yetzer', 18, 3),
+      ('Jenny', 'Gump', 11, 4),
+      ('Johnny', 'Depp', 14, 5),
+      ('Darth', 'Vader', 17, 6),
+      ('Luke', 'Skywalker', 3, 1);
+       
